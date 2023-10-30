4dec0adf1ade78905b59568b94583337dd9821d8
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-31 00:20:54 +0200

    add email service

---
+++ Herokume.Application/Contracts/Infrastrcture/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using Herokume.Application.Models.Mail;
+
+namespace Herokume.Application.Contracts.Infrastrcture;
+
+public interface IEmailService
+{
+    public Task<bool> SendEmail(Email email);
+}
