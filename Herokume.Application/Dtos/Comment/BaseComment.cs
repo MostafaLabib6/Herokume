659974b86cb1bef65a73ba312b7550b5277d21b3
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-31 16:13:36 +0200

    add comment outerface layer

---
+++ Herokume.Application/Dtos/Comment/BaseComment.cs
@@ -0,0 +1,8 @@
+﻿namespace Herokume.Application.Dtos.Comment
+{
+    public class BaseComment:BaseDto
+    {
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ Herokume.Application/Dtos/Comment/CommentsListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Herokume.Application.Dtos.Comment;
+
+public class CommentsListDto:BaseDto
+{
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public List<BaseComment>? Responses { get; set; } //replaies to comment.
+}
