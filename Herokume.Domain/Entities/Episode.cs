c3afce7a681532fdf313a73e4ee1264822c27290
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-31 16:16:51 +0200

    update epsisode entity

--- Herokume.Domain/Entities/Episode.cs
+++ Herokume.Domain/Entities/Episode.cs
@@ -7,7 +7,10 @@ public class Episode
     public string Name { get; set; } = string.Empty;
     public int Length { get; set; }
     public string? EpisodeURL { get; set; }
+    public int Likes { get; set; }
     public int EpisodeNumber { get; set; }
+    public float Rating { get; set; }
     public Series Series { get; set; }
+    List<Comment>? Comments { get; set; }
 
 }
