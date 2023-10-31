34aa139320a13c2d1492a41c9f4a4f15a4fcb279
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-31 16:13:48 +0200

    add Episode outerface layer

---
+++ Herokume.Application/Dtos/Episode/EpisodeDetailsDto.cs
@@ -0,0 +1,18 @@
+﻿using Herokume.Application.Dtos.Comment;
+using Herokume.Application.Dtos.Series;
+using Herokume.Domain.Entities;
+
+namespace Herokume.Application.Dtos.Episode;
+
+public class EpisodeDetailsDto:BaseDto
+{
+    public string Name { get; set; } = string.Empty;
+    public int Length { get; set; }
+    public string? EpisodeURL { get; set; }
+    public int Likes { get; set; }
+    public int EpisodeNumber { get; set; }
+    public float Rating { get; set; }
+    public BaseSeriesDto Series { get; set; }
+    List<CommentsListDto>? Comments { get; set; }
+
+}
---
+++ Herokume.Application/Dtos/Episode/EpisodeListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Herokume.Application.Dtos.Episode;
+
+public class EpisodeListDto:BaseDto
+{
+    public string Name { get; set; } = string.Empty;
+    public int Length { get; set; }
+    public string? EpisodeURL { get; set; }
+}
