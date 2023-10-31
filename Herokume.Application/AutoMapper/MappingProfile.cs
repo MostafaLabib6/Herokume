2f490cd732a198d9763dd06ac33035be87d2dada
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-31 16:12:26 +0200

    add mapping profile

---
+++ Herokume.Application/AutoMapper/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Herokume.Application.Dtos.Category;
+using Herokume.Application.Dtos.Comment;
+using Herokume.Application.Dtos.Episode;
+using Herokume.Application.Dtos.Series;
+using Herokume.Domain.Entities;
+
+namespace Herokume.Application.AutoMapper;
+public class MappingProfile:Profile
+{
+    public MappingProfile()
+    {
+        //Mapping for Series
+        CreateMap<Series,SeriesDetailsDto>().ReverseMap();
+        CreateMap<Series,SeriesListDto>();
+
+        //Mapping for Episodes
+        CreateMap<Episode,EpisodeDetailsDto>().ReverseMap();
+        CreateMap<Episode,EpisodeListDto>();
+
+        //Mapping for Comments
+        CreateMap<Comment,CommentsListDto>();
+        
+        //Mapping for Categories
+        CreateMap<Category,CategoryDto>();
+        CreateMap<Category,CategoryListDto>();
+
+    }
+}
+
