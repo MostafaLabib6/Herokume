afd6e19ccb665f31aa2f4f0ebee4467cbe2ae919
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-31 21:31:30 +0200

    Create Unit of work

---
+++ Herokume.Application/Contracts/Persistance/IUnitofWork.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Herokume.Application.Contracts.Persistance;
+
+public interface IUnitofWork
+{
+    public ISeriesRepository SeriesRepository { get; set; }
+    public ICommentRepository CommentRepository{ get; set; }
+    public ICategoryRepository CategoryRepository { get; set; }
+    public IEpisodeRepository EpisodeRepository { get; set; }
+    public ITagRepository TagRepository { get; set; }
+    //TODO: adding user repsoitory
+}
