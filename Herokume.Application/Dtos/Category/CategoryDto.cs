b9196d828327af2789e7a1b8d7984e72e25ce3e0
MostafaLabib6 <mostafalabib510@gmail.com>
2023-10-31 16:13:19 +0200

    add category Outerface layer

---
+++ Herokume.Application/Dtos/Category/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using Herokume.Application.Dtos.Series;
+
+namespace Herokume.Application.Dtos.Category;
+
+public class CategoryDto:BaseDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public List<SeriesListDto>? Series { get; set; }
+}
