b22aa0bd62920b8731b43ec3d45e6779e4e6c60e
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-02 00:13:03 +0200

    add category validators

---
+++ Herokume.Application/Dtos/Category/Validators/CategoryListDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+
+namespace Herokume.Application.Dtos.Category.Validators
+{
+    public class CategoryListDtoValidator:AbstractValidator<CategoryListDto>
+    {
+        public CategoryListDtoValidator()
+        {
+              RuleFor(category => category.Name)
+            .NotEmpty().WithMessage("{PropertyName} is Required")
+            .MaximumLength(50).WithMessage("{PropertyName} must not exceed {ComparisonVa1ue} characters.")
+            .NotNull();
+        }
+      
+    }
+}
---
+++ Herokume.Application/Dtos/Category/Validators/CreateCategoryDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Herokume.Application.Dtos.Category.Validators;
+
+public class CreateCategoryDtoValidator : AbstractValidator<CreateCategoryDto>
+{
+    public CreateCategoryDtoValidator()
+    {
+        RuleFor(category => category.Name)
+            .NotEmpty().WithMessage("{PropertyName} is Required")
+            .MaximumLength(50).WithMessage("{PropertyName} must not exceed {ComparisonVa1ue} characters.")
+            .NotNull();
+    }
+}
