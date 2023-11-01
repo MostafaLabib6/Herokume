47e2d9ad23d4c855692d8f35f3e4be40cbfbc165
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-02 00:15:49 +0200

    update category command handler

--- Herokume.Application/Features/Commands/Category/Handler/CreateCategoryHandler.cs
+++ Herokume.Application/Features/Commands/Category/Handler/CreateCategoryHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Herokume.Application.Contracts.Persistance;
+using Herokume.Application.Dtos.Category.Validators;
+using Herokume.Application.Exceptions;
 using Herokume.Application.Features.Commands.Category.Request;
 using MediatR;
 
@@ -19,6 +21,11 @@ public class CreateCategoryHandler : IRequestHandler<CreateCategory, Unit>
 
     public async Task<Unit> Handle(CreateCategory request, CancellationToken cancellationToken)
     {
+        var validator = new CreateCategoryDtoValidator();
+        var validatorResualt =await  validator.ValidateAsync(request.CreateCategoryDto, cancellationToken);
+
+        if(!validatorResualt.IsValid)
+            throw new Exception();
         var category = _mapper.Map<Domain.Entities.Category>(request.CreateCategoryDto);
         _categoryRepository.Add(category);
         bool valid = await _categoryRepository.Save();
