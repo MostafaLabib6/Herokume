66658f352ea4982cd327eb324bc92d4bc5741155
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-03 23:51:01 +0200

    add photo service

--- Herokume.Application/Contracts/Infrastrcture/PhotoService/IPhotoService.cs
+++ Herokume.Application/Contracts/Infrastrcture/PhotoService/IPhotoService.cs
@@ -1,5 +1,10 @@
-﻿namespace Herokume.Application.Contracts.Infrastrcture.PhotoService;
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+
+namespace Herokume.Application.Contracts.Infrastrcture.PhotoService;
 
 public interface IPhotoService
 {
+    public Task<ImageUploadResult> AddImage(IFormFile file);
+    public Task<DeletionResult> DeleteImage(string publicId);
 }
