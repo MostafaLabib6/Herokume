2baa58efe4a67f6d040832bc24b83d3ace664bc0
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-03 23:52:15 +0200

    add cloudinary photo settings

---
+++ Herokume.Application/Models/Photo/PhotoSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Herokume.Application.Models.Photo;
+
+public class PhotoSettings
+{
+    public string CloudName { get; set; }
+    public string ApiKey { get; set; }
+    public string ApiSecret { get; set; }
+}
