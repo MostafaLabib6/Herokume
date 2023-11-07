b0d71d4d22243b919112d313256e38a56345c368
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-07 21:24:53 +0200

    add impleamentation for unitof work

---
+++ Herokume.Persisitance/Repositories/UnitofWork.cs
@@ -0,0 +1,12 @@
+﻿using Herokume.Application.Contracts.Persistance;
+
+namespace Herokume.Persisitance.Repositories;
+
+public class UnitofWork : IUnitofWork
+{
+    public ISeriesRepository SeriesRepository { get; set; }
+    public ICommentRepository CommentRepository { get; set; }
+    public ICategoryRepository CategoryRepository { get; set; }
+    public IEpisodeRepository EpisodeRepository { get; set; }
+    public ITagRepository TagRepository { get; set; }
+}
