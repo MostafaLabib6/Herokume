a26fe5090a444d635bb7f009bb3dd92470455155
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-08 23:47:13 +0200

    Rename foreignkey for episodes

--- Herokume.Persisitance/Migrations/20231107215414_updaateEpisodeTable.Designer.cs
+++ Herokume.Persisitance/Migrations/20231107215414_updaateEpisodeTable.Designer.cs
@@ -4,6 +4,7 @@ using Herokume.Persisitance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Herokume.Persisitance.Migrations
 {
     [DbContext(typeof(HerokumeDbContext))]
-    partial class HerokumeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231107215414_updaateEpisodeTable")]
+    partial class updaateEpisodeTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Herokume.Persisitance/Migrations/20231107215414_updaateEpisodeTable.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Herokume.Persisitance.Migrations
+{
+    /// <inheritdoc />
+    public partial class updaateEpisodeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Episodes_Series_SeriesID",
+                table: "Episodes");
+
+            migrationBuilder.RenameColumn(
+                name: "SeriesID",
+                table: "Episodes",
+                newName: "SeriesId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Episodes_SeriesID",
+                table: "Episodes",
+                newName: "IX_Episodes_SeriesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Episodes_Series_SeriesId",
+                table: "Episodes",
+                column: "SeriesId",
+                principalTable: "Series",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Episodes_Series_SeriesId",
+                table: "Episodes");
+
+            migrationBuilder.RenameColumn(
+                name: "SeriesId",
+                table: "Episodes",
+                newName: "SeriesID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Episodes_SeriesId",
+                table: "Episodes",
+                newName: "IX_Episodes_SeriesID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Episodes_Series_SeriesID",
+                table: "Episodes",
+                column: "SeriesID",
+                principalTable: "Series",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
