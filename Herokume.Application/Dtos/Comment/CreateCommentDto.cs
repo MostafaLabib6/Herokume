1f2caa7988ccca081694cef486a70308d9701d9d
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-08 23:49:41 +0200

    add userid,episodeid and series id

--- Herokume.Application/Dtos/Comment/CreateCommentDto.cs
+++ Herokume.Application/Dtos/Comment/CreateCommentDto.cs
@@ -4,6 +4,10 @@ namespace Herokume.Application.Dtos.Comment
 {
     public class CreateCommentDto
     {
-        public string Content { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public Guid UserId { get; set; } = Guid.Empty;
+        public Guid SeriesId { get; set; } = Guid.Empty;
+        public Guid EpisodeId { get; set; } = Guid.Empty;
+
     }
 }
