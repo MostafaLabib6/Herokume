299845e3e238bff53165a2525dff87cab2d54559
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-12 00:58:28 +0200

    add authentication service

---
+++ Herokume.Application/Contracts/Infrastrcture/IdentityService/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using Herokume.Application.Models.Identity;
+using Herokume.Infrastrcture.Authentication;
+
+namespace Herokume.Application.Contracts.Infrastrcture.IdentityService;
+
+public interface IAuthenticationService
+{
+    public Task<bool> IsAuthenticated(string email);
+    public string GenerateToken(ApplicationUser user);
+    public Task Logout();
+    public Task<bool> CreateUser(ApplicationUser user);
+    Task<AuthResult> Login(LoginUserRequestDto user);
+    Task<AuthResult> Register(RegisterUserRequestDto user);
+}
