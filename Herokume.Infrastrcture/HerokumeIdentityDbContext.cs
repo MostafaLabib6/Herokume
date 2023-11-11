6fe41cd22f986224bf0f3155167a2b89eded565a
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-12 01:02:08 +0200

    add idnetitydbcontext and register it;

---
+++ Herokume.Infrastrcture/HerokumeIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Herokume.Infrastrcture.Authentication;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Herokume.Infrastrcture;
+
+public class HerokumeIdentityDbContext : IdentityDbContext<ApplicationUser>
+{
+    public HerokumeIdentityDbContext(DbContextOptions<HerokumeIdentityDbContext> options) : base(options)
+    {
+    }
+
+}
