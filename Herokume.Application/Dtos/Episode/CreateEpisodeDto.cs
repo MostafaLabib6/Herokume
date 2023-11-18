5855686de46753217750f9daa24b876063f06194
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-18 19:50:25 +0200

    add more details for Creation of Episode

--- Herokume.Application/Dtos/Episode/CreateEpisodeDto.cs
+++ Herokume.Application/Dtos/Episode/CreateEpisodeDto.cs
@@ -6,5 +6,8 @@ public class CreateEpisodeDto
 {
     public string Name { get; set; } = string.Empty;
     public string? EpisodeURL { get; set; }
-    public BaseSeriesDto Series { get; set; }
+    public int EpisodeNumber { get; set; } = 0;
+    public int Length { get; set; } = 25;
+    public DateTime? CreatedAt { get; set; } = DateTime.Now;
+    //public BaseSeriesDto Series { get; set; } can set it ez. Cause it's created before episodes.
 }
