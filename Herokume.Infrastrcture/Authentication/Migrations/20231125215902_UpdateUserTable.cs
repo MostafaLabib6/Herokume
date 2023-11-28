0c62fb5e6988f09d23937a19397a5e49afa72235
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-29 00:38:07 +0200

    authentication migrations

---
+++ Herokume.Infrastrcture/Authentication/Migrations/20231125215902_UpdateUserTable.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Herokume.Infrastrcture.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "AspNetUsers",
+                type: "varchar(200)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FavoriteMovie",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Hobby",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoleName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Series",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NormalizeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShowInSlides = table.Column<bool>(type: "bit", nullable: false),
+                    Trailer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rating = table.Column<float>(type: "real", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Likes = table.Column<int>(type: "int", nullable: false),
+                    Views = table.Column<int>(type: "int", nullable: false),
+                    AddToWatchList = table.Column<bool>(type: "bit", nullable: false),
+                    SeasonNumber = table.Column<int>(type: "int", nullable: false),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Series", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Series_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tag",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TagName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategorySeries",
+                columns: table => new
+                {
+                    SeriesID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    categoriesID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategorySeries", x => new { x.SeriesID, x.categoriesID });
+                    table.ForeignKey(
+                        name: "FK_CategorySeries_Category_categoriesID",
+                        column: x => x.categoriesID,
+                        principalTable: "Category",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategorySeries_Series_SeriesID",
+                        column: x => x.SeriesID,
+                        principalTable: "Series",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Episode",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Length = table.Column<int>(type: "int", nullable: false),
+                    EpisodeURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Likes = table.Column<int>(type: "int", nullable: false),
+                    EpisodeNumber = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<float>(type: "real", nullable: false),
+                    SeriesId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Episode", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Episode_Series_SeriesId",
+                        column: x => x.SeriesId,
+                        principalTable: "Series",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RelatedSeries",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rating = table.Column<float>(type: "real", nullable: false),
+                    SeriesID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RelatedSeries", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_RelatedSeries_Series_SeriesID",
+                        column: x => x.SeriesID,
+                        principalTable: "Series",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SeriesTag",
+                columns: table => new
+                {
+                    SeriesID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TagsID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeriesTag", x => new { x.SeriesID, x.TagsID });
+                    table.ForeignKey(
+                        name: "FK_SeriesTag_Series_SeriesID",
+                        column: x => x.SeriesID,
+                        principalTable: "Series",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SeriesTag_Tag_TagsID",
+                        column: x => x.TagsID,
+                        principalTable: "Tag",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SeriesID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    EpisodeID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ResponseToID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    Likes = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Comment_Comment_ResponseToID",
+                        column: x => x.ResponseToID,
+                        principalTable: "Comment",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_Comment_Episode_EpisodeID",
+                        column: x => x.EpisodeID,
+                        principalTable: "Episode",
+                        principalColumn: "ID");
+                    table.ForeignKey(
+                        name: "FK_Comment_Series_SeriesID",
+                        column: x => x.SeriesID,
+                        principalTable: "Series",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategorySeries_categoriesID",
+                table: "CategorySeries",
+                column: "categoriesID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_EpisodeID",
+                table: "Comment",
+                column: "EpisodeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_ResponseToID",
+                table: "Comment",
+                column: "ResponseToID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_SeriesID",
+                table: "Comment",
+                column: "SeriesID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Episode_SeriesId",
+                table: "Episode",
+                column: "SeriesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RelatedSeries_SeriesID",
+                table: "RelatedSeries",
+                column: "SeriesID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Series_ApplicationUserId",
+                table: "Series",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SeriesTag_TagsID",
+                table: "SeriesTag",
+                column: "TagsID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategorySeries");
+
+            migrationBuilder.DropTable(
+                name: "Comment");
+
+            migrationBuilder.DropTable(
+                name: "RelatedSeries");
+
+            migrationBuilder.DropTable(
+                name: "SeriesTag");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "Episode");
+
+            migrationBuilder.DropTable(
+                name: "Tag");
+
+            migrationBuilder.DropTable(
+                name: "Series");
+
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FavoriteMovie",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Hobby",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RoleName",
+                table: "AspNetUsers");
+        }
+    }
+}
