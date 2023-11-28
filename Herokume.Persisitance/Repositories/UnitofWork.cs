5295939831570edd5161a41452ee148c2529c3f9
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-29 00:40:40 +0200

    impleament unit of work service

--- Herokume.Persisitance/Repositories/UnitofWork.cs
+++ Herokume.Persisitance/Repositories/UnitofWork.cs
@@ -1,12 +1,38 @@
 ﻿using Herokume.Application.Contracts.Persistance;
+using Herokume.Infrastrcture;
+using Microsoft.AspNetCore.Http;
 
 namespace Herokume.Persisitance.Repositories;
 
 public class UnitofWork : IUnitofWork
 {
-    public ISeriesRepository SeriesRepository { get; set; }
-    public ICommentRepository CommentRepository { get; set; }
-    public ICategoryRepository CategoryRepository { get; set; }
-    public IEpisodeRepository EpisodeRepository { get; set; }
-    public ITagRepository TagRepository { get; set; }
+    private readonly HerokumeDbContext _context;
+    private ISeriesRepository _seriesRepository;
+    private ICategoryRepository _categoryRepository;
+    private IEpisodeRepository _episodeRepository;
+    private ICommentRepository _commentRepository;
+    private ITagRepository _tagRepository;
+
+    public UnitofWork(HerokumeDbContext context, IHttpContextAccessor httpContextAccessor)
+    {
+        _context = context ?? throw new ArgumentNullException();
+    }
+
+    public ISeriesRepository SeriesRepository =>
+        _seriesRepository ??= new SeriesRepository(_context);
+    public ICommentRepository CommentRepository =>
+        _commentRepository ??= new CommentRepository(_context);
+    public ICategoryRepository CategoryRepository =>
+        _categoryRepository = new CategoryRepository(_context);
+    public IEpisodeRepository EpisodeRepository =>
+        _episodeRepository ??= new EpisodeRepository(_context);
+    public ITagRepository TagRepository =>
+        _tagRepository;
+
+    public void Dispose()
+    {
+        _context?.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
 }
