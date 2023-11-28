48d1c2b7263c1453ad3e12177c3467da5f2f594e
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-29 00:43:55 +0200

    update User identity

--- Herokume.Application/Models/Identity/ApplicationUser.cs
+++ Herokume.Application/Models/Identity/ApplicationUser.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Herokume.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
 namespace Herokume.Infrastrcture.Authentication;
 
 public class ApplicationUser : IdentityUser
 {
-    //public string Avatar { get; set; }
+    public string? Avatar { get; set; }
+    public string? FavoriteMovie { get; set; }
+    public string? Hobby { get; set; }
+    public string? RoleName { get; set; }
+    public List<Series>? LikedMovies { get; set; }
 }
