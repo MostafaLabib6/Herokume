8d4ca560febd266b0cf9366f03108da3a952b934
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-29 00:37:00 +0200

    update email service by using MIMEMessage

--- Herokume.Infrastrcture/Mail/EmailService.cs
+++ Herokume.Infrastrcture/Mail/EmailService.cs
@@ -1,8 +1,10 @@
-﻿using Herokume.Application.Contracts.Infrastrcture.EmailService;
+﻿using Azure.Core;
+using Herokume.Application.Contracts.Infrastrcture.EmailService;
 using Herokume.Application.Models.Mail;
+using MailKit.Security;
 using Microsoft.Extensions.Options;
+using MimeKit;
 using Serilog;
-using Serilog.Core;
 using System.Net;
 using System.Net.Mail;
 
@@ -10,33 +12,61 @@ namespace Herokume.Infrastrcture.Mail;
 
 public class EmailService : IEmailService
 {
-    private readonly EmailSetting _emailSettings;
-    //private readonly ILogger _logger;
+    private readonly EmailSetting _mailSettings;
+    private readonly ILogger _logger;
 
     public EmailService(
-        IOptions<EmailSetting> emailSettings
-        //ILogger logger
+        IOptions<EmailSetting> emailSettings,
+        ILogger logger
         )
     {
-        _emailSettings = emailSettings.Value;
-        //_logger = logger;
+        _mailSettings = emailSettings.Value;
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
     public async Task SendEmail(Email email)
     {
-        var smtpClient = new SmtpClient("smtp.gmail.com")
-        {
-            Port = 587,
-            Credentials = new NetworkCredential(_emailSettings.FromAddress, _emailSettings.ApiKey),
-            EnableSsl = true,
-            UseDefaultCredentials = true
-        };
+        //var smtpClient = new SmtpClient("smtp.gmail.com")
+        //{
+        //    EnableSsl = true,
+        //    Credentials = new NetworkCredential(_emailSettings.FromAddress, _emailSettings.ApiKey),
+        //    UseDefaultCredentials = false,
+        //    Port = 587,
+
+        //};
+        //try
+        //{
+        //    await smtpClient.SendMailAsync(new MailMessage(from: _emailSettings.FromAddress, to: email.To, subject: email.Subject, email.Body));
+        //    _logger.Information($"Email sent to {email.To} with subject: {email.Subject}");
+        //}
+        //catch (Exception ex)
+        //{
+        //    _logger.Error(ex, "Failed to send email.");
+        //}
+
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress(_mailSettings.FromAddress, _mailSettings.FromAddress));
+        message.To.Add(MailboxAddress.Parse(email.To));
+        message.Subject = email.Subject;
+
+        var builder = new BodyBuilder();
+        builder.HtmlBody = email.Body;
+        message.Body = builder.ToMessageBody();
+
+        using var smtp = new MailKit.Net.Smtp.SmtpClient();
         try
         {
-            await smtpClient.SendMailAsync(new MailMessage(from: _emailSettings.FromAddress, to: email.To, subject: email.Subject, email.Body));
+            smtp.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.FromAddress, _mailSettings.ApiKey);
+            await smtp.SendAsync(message);
         }
-        catch (Exception ex)
+        catch
         {
-            //_logger.Error(ex, "Failed to send email.");
+            Directory.CreateDirectory("mailssave");
+            var emailsavefile = string.Format(@"mailssave/{0}.eml", Guid.NewGuid());
+            await message.WriteToAsync(emailsavefile);
         }
+
+        smtp.Disconnect(true);
     }
+
 }
