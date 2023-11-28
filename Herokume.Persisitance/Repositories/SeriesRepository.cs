c48723b6f10c012606fe5b4d8a4b7187125c27c6
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-29 00:42:51 +0200

    add service Geting Random series and get number of episode in series

--- Herokume.Persisitance/Repositories/SeriesRepository.cs
+++ Herokume.Persisitance/Repositories/SeriesRepository.cs
@@ -14,9 +14,31 @@ public class SeriesRepository : GenaricRepository<Series>, ISeriesRepository
         _dbContext = dbContext;
     }
 
-    public Task<List<Series>> GetRandomSeries(int count = 10)
+    public async Task<int> GetNumberofEpisodesinSeries(Guid seriesId)
     {
-        throw new NotImplementedException();
+        var series = await _dbContext.Series
+              .Include(s => s.Episodes)
+              .FirstOrDefaultAsync(s => s.ID == seriesId);
+
+        var count = series?.Episodes?.Count() ?? 0;
+        return count;
+    }
+
+    public async Task<List<Series>> GetRandomSeries(int count = 10)
+    {
+        var ids = await GetRandomSeriesIds(count);
+        return await _dbContext.Series.Where(x => ids.Contains(x.ID)).ToListAsync();
+    }
+
+    private async Task<List<Guid>> GetRandomSeriesIds(int count = 1)
+    {
+        var random = new Random();
+        var allseriesIds = await _dbContext.Series.Select(x => x.ID).ToListAsync();
+
+        count = Math.Min(count, allseriesIds.Count());
+        var randomSeriesIds = allseriesIds.OrderBy(x => random.Next()).Take(count).ToList();
+
+        return randomSeriesIds;
     }
 
     public async Task<Series> GetSeriesDetails(Guid id)
