5534daf04daa4f3fa39e4b59a91cce02b34506a8
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-30 15:50:39 +0200

    create user repository

---
+++ Herokume.Persisitance/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Herokume.Application.Contracts.Persistance;
+using Herokume.Infrastrcture;
+using Herokume.Infrastrcture.Authentication;
+namespace Herokume.Persisitance.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly HerokumeIdentityDbContext _dbContext;
+
+    public UserRepository(HerokumeIdentityDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+}
