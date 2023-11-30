ef3643c0a8be3fcb38badd90c75d7faf1ef7fec8
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-30 15:49:30 +0200

    update file formate

--- Herokume.Persisitance/Repositories/CommentRepository.cs
+++ Herokume.Persisitance/Repositories/CommentRepository.cs
@@ -2,30 +2,21 @@
 using Herokume.Application.Dtos.Comment;
 using Herokume.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Herokume.Persisitance.Repositories
+
+namespace Herokume.Persisitance.Repositories;
+
+public class CommentRepository : GenaricRepository<Comment>, ICommentRepository
 {
-    public class CommentRepository : GenaricRepository<Comment>, ICommentRepository
+    private readonly HerokumeDbContext _dbContext;
+
+    public CommentRepository(HerokumeDbContext dbContext) : base(dbContext)
     {
-        private readonly HerokumeDbContext _dbContext;
-
-        public CommentRepository(HerokumeDbContext dbContext) : base(dbContext)
-        {
-            _dbContext = dbContext;
-        }
-        public Task<BaseComment> CreateComment { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public async Task<List<Comment>> GetCommentsWithReplies() =>
-            await _dbContext.Comments.Include(x=>x.Responses).ToListAsync();
-
-        //Task<BaseComment> ICommentRepository.CreateComment()
-        //{
-        //    //TODO:
-        //}
+        _dbContext = dbContext;
     }
+    public Task<BaseComment> CreateComment { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public async Task<List<Comment>> GetCommentsWithReplies() =>
+        await _dbContext.Comments.Include(x => x.Responses).ToListAsync();
+
 }
