8af43e685d2e1532ac43c18f8717320b4f5a7831
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-30 15:48:41 +0200

    update comment entity

--- Herokume.Domain/Entities/Comment.cs
+++ Herokume.Domain/Entities/Comment.cs
@@ -1,23 +1,13 @@
 ﻿using Herokume.Domain.Common;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Herokume.Domain.Entities;
-
 public class Comment : BaseEntity
 {
     public string Content { get; set; } = string.Empty;
     public Series? Series { get; set; } = new Series();
     public Episode? Episode { get; set; } = new Episode();
-    public float Rating { get; set; } = float.MinValue;
-    //TODO: adding user .    
+    public Guid UserId { get; set; } = Guid.NewGuid();
     public Comment? ResponseTo { get; set; } = new();
-    public ICollection<CommentResponse> Responses { get; set; } = new List<CommentResponse>(); //replaies to comment.
-    public int Likes { get; set; } = int.MinValue;
-}
-
-public class CommentResponse : BaseEntity
-{
-    public string Text { get; set; } = string.Empty;
-    public Guid CommentId { get; set; } = Guid.NewGuid();
-    public Comment Comment { get; set; } = new Comment();
+    public ICollection<Comment>? Responses { get; set; } = new List<Comment>(); //replaies to comment.
+    public int Likes { get; set; } = 0;
+    public bool IsSeries { get; set; }
 }
