42913a9301fe503002147060d31b0d2c2595361a
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-30 15:51:38 +0200

    update save changes to track only the first update entity ??

--- Herokume.Persisitance/HerokumeDbContext.cs
+++ Herokume.Persisitance/HerokumeDbContext.cs
@@ -1,30 +1,39 @@
-﻿using Herokume.Application.Dtos.Series;
-using Herokume.Domain.Common;
+﻿using Herokume.Domain.Common;
 using Herokume.Domain.Entities;
+using Herokume.Persisitance.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Herokume.Persisitance;
 
 public class HerokumeDbContext : DbContext
 {
+
     public HerokumeDbContext(DbContextOptions<HerokumeDbContext> options) : base(options)
     {
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-    }
-    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
-    {
-        foreach (var entity in ChangeTracker.Entries<BaseEntity>())
-        {
-            entity.Entity.ModifiedAt = DateTime.Now;
-            if (entity.State == EntityState.Added)
-                entity.Entity.CreatedAt = DateTime.Now;
-        }
-        return base.SaveChangesAsync(cancellationToken);
+        //        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CategoryConfiguration).Assembly);
+        // modelBuilder.Entity<Category>().HasData(category1, category2);
 
     }
-    // 
+    public virtual async Task<int> SaveChangesAsync()
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+            .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+        {
+            entry.Entity.ModifiedAt = DateTime.Now;
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.Now;
+            }
+            break;
+        }
+
+        var result = await base.SaveChangesAsync();
+
+        return result;
+    }
 
 
     public DbSet<Series> Series { get; set; }
@@ -32,6 +41,5 @@ public class HerokumeDbContext : DbContext
     public DbSet<Comment> Comments { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Tag> Tags { get; set; }
-    public DbSet<CommentResponse> CommentResponses { get; set; }
     public DbSet<RelatedSeries> RelatedSeries { get; set; }
 }
