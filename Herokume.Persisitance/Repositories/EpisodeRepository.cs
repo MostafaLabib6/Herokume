b75be808fdea74e1818b329fb3b90ba848368115
MostafaLabib6 <mostafalabib510@gmail.com>
2023-11-30 15:49:56 +0200

    update file format

--- Herokume.Persisitance/Repositories/EpisodeRepository.cs
+++ Herokume.Persisitance/Repositories/EpisodeRepository.cs
@@ -1,35 +1,28 @@
 ﻿using Herokume.Application.Contracts.Persistance;
 using Herokume.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace Herokume.Persisitance.Repositories;
 
-namespace Herokume.Persisitance.Repositories
+public class EpisodeRepository : GenaricRepository<Episode>, IEpisodeRepository
 {
-    public class EpisodeRepository : GenaricRepository<Episode>, IEpisodeRepository
+    private readonly HerokumeDbContext _dbContext;
+
+    public EpisodeRepository(HerokumeDbContext dbContext) : base(dbContext)
     {
-        private readonly HerokumeDbContext _dbContext;
+        _dbContext = dbContext;
+    }
+    public async Task<Episode> GetEpisodeDetails(Guid id)
+    {
+        var episodes = _dbContext.Episodes as IQueryable<Episode>;
+        return await episodes.Include(x => x.Series)
+            .ThenInclude(x => x.Comments).OrderBy(x => x.EpisodeNumber)
+            .FirstOrDefaultAsync(x => x.ID == id);
+    }
 
-        public EpisodeRepository(HerokumeDbContext dbContext) : base(dbContext)
-        {
-            _dbContext = dbContext;
-        }
-        public async Task<Episode> GetEpisodeDetails(Guid id)
-        {
-            var episodes = _dbContext.Episodes as IQueryable<Episode>;
-            return await episodes.Include(x => x.Series)
-                .Include(x => x.Comments)
-                .FirstOrDefaultAsync(x => x.ID == id);
-        }
+    public async Task<List<Episode>> GetSeriesEpisodes(Guid seriesId)
+    {
+        var episodes = _dbContext.Episodes as IQueryable<Episode>;
+        return await episodes.Where(x => x.SeriesId == seriesId).ToListAsync();
 
-        public async Task<List<Episode>> GetSeriesEpisodes(Guid seriesId)
-        {
-            var episodes = _dbContext.Episodes as IQueryable<Episode>;
-            return await episodes.Where(x => x.SeriesId == seriesId).ToListAsync();
-
-        }
     }
 }
