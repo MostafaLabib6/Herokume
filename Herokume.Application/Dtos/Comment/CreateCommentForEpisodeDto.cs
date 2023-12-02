a0a6edf71fc73ec3ca6d48d4574784108a2bd08f
MostafaLabib6 <mostafalabib510@gmail.com>
2023-12-03 00:23:12 +0200

    suberate dto comment's creation to be for series and episode

--- Herokume.Application/Dtos/Comment/CreateCommentForEpisodeDto.cs
+++ Herokume.Application/Dtos/Comment/CreateCommentForEpisodeDto.cs
@@ -2,12 +2,11 @@
 
 namespace Herokume.Application.Dtos.Comment
 {
-    public class CreateCommentDto
+    public class CreateCommentForEpisodeDto
     {
         public string Content { get; set; } = string.Empty;
         public Guid UserId { get; set; } = Guid.Empty;
-        public Guid SeriesId { get; set; } = Guid.Empty;
-        public Guid EpisodeId { get; set; } = Guid.Empty;
+        public Guid? EpisodeId { get; set; } = Guid.Empty;
 
     }
 }
---
+++ Herokume.Application/Dtos/Comment/CreateCommentForSeriesDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Herokume.Application.Dtos.Comment
+{
+    public class CreateCommentForSeriesDto
+    {
+        public string Content { get; set; } = string.Empty;
+        public Guid UserId { get; set; } = Guid.Empty;
+        public Guid? SeriesId { get; set; } = Guid.Empty;
+
+    }
+}
