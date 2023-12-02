707c52b556facaaccd5de639e8d8474313baba90
MostafaLabib6 <mostafalabib510@gmail.com>
2023-12-03 00:21:24 +0200

    update Validator of Create comment to validate the creation for series and episodes comments

---
+++ Herokume.Application/Dtos/Comment/Validator/CreateCommentForEpisodeDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Herokume.Application.Dtos.Comment.Validator;
+
+public class CreateCommentForEpisodeDtoValidator : AbstractValidator<CreateCommentForEpisodeDto>
+{
+    public CreateCommentForEpisodeDtoValidator()
+    {
+        RuleFor(comment => comment.Content)
+           .NotEmpty().WithMessage("{PropertyName} is Required")
+           .MaximumLength(1000).WithMessage("{PropertyName} must not exceed {ComparisonValue} characters.")
+           .NotNull();
+    }
+}
---
+++ Herokume.Application/Dtos/Comment/Validator/CreateCommentForSeriesDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Herokume.Application.Dtos.Comment.Validator;
+
+public class CreateCommentForSeriesDtoValidator:AbstractValidator<CreateCommentForSeriesDto>
+{
+    public CreateCommentForSeriesDtoValidator()
+    {
+         RuleFor(comment => comment.Content)
+            .NotEmpty().WithMessage("{PropertyName} is Required")
+            .MaximumLength(1000).WithMessage("{PropertyName} must not exceed {ComparisonValue} characters.")
+            .NotNull();
+    }
+}
