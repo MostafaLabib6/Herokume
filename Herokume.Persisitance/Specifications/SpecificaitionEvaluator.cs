daa14ede3f0d4c832089b99f77a950c54c5427d4
Mostafa Labib <mostafal@ejada.com>
2024-07-06 16:55:22 +0300

    [Feature] Apply Specification Pattern to enhance readability.

---
+++ Herokume.Persisitance/Specifications/SpecificaitionEvaluator.cs
@@ -0,0 +1,42 @@
+﻿using Herokume.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Herokume.Persisitance.Specifications
+{
+    public class SpecificaitionEvaluator
+    {
+        public static IQueryable<TEntity> GetQuery<TEntity>(IQueryable<TEntity> inputQuery, Specification<TEntity> specification)
+            where TEntity : BaseEntity
+        {
+            var query = inputQuery;
+
+            if (specification.Cratiria != null)
+            {
+                query = query.Where(specification.Cratiria);
+            }
+
+            query = specification.Includes?.Aggregate(query, (current, include) => current.Include(include));
+
+            if (specification.OrderBy != null)
+            {
+                query = query.OrderBy(specification.OrderBy);
+            }
+            else if (specification.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(specification.OrderByDescending);
+            }
+
+            if (specification.IsPagingEnabled)
+            {
+                query = query.Skip(specification.Skip)
+                    .Take(specification.Take);
+            }
+            if (specification.IsSplitQuery)
+            {
+                query = query.AsSplitQuery();
+            }
+
+            return query;
+        }
+    }
+}
---
+++ Herokume.Persisitance/Specifications/Specification.cs
@@ -0,0 +1,45 @@
+﻿using Herokume.Domain.Common;
+using System.Linq.Expressions;
+
+namespace Herokume.Persisitance.Specifications
+{
+    public abstract class Specification<TEntity>
+        where TEntity : BaseEntity
+    {
+
+        public Expression<Func<TEntity, bool>>? Cratiria { get; }
+
+        public List<Expression<Func<TEntity, object>>>? Includes { get; } = new();
+        public Expression<Func<TEntity, object>>? OrderBy { get; private set; }
+        public Expression<Func<TEntity, object>>? OrderByDescending { get; private set; }
+
+        public bool IsSplitQuery { get; protected set; } = false;
+        public bool IsPagingEnabled { get; private set; } = false;
+        public int Take { get; private set; } = 0;
+        public int Skip { get; private set; } = 0;
+
+
+        public Specification(Expression<Func<TEntity, bool>> cratiria)
+        {
+            Cratiria = cratiria;
+        }
+
+        public void AddInclude(Expression<Func<TEntity, object>> include)
+            => Includes.Add(include);
+
+        public void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
+        }
+
+        public void ApplyOrderBy(Expression<Func<TEntity, object>> orderBy)
+            => OrderBy = orderBy;
+
+        public void ApplyOrderByDescending(Expression<Func<TEntity, object>> orderByDescending)
+            => OrderByDescending = orderByDescending;
+
+
+    }
+}
